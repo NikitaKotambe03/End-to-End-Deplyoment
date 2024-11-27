0f767afc227d1525ab1bc15b30dffd5e689b7ffd
NikitaKotambe03 <kotambe.nikita.16ce7041@gmail.com>
2024-11-27 23:46:28 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  subnet_id     = "subnet-xxxxxxx"
+  subnet_id     = "subnet-0d1e1f0b5338e462f"
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = "Jenkins_Server.pem" 
